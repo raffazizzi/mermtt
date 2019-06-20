1c710a518012606b7f1ab3df1db5888b4001d7b5
raffazizzi <raffaeleviglianti@gmail.com>
2019-06-20 17:50:46 -0400

    first dev

---
+++ precache-manifest.a573f45e1ea8c3c9a1191e44d033b318.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10d810aea0f72228d9aed1860382fde5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f99cd4666b1eae75b039",
+    "url": "/static/css/main.8c8f51b2.chunk.css"
+  },
+  {
+    "revision": "d466581e2eaa77065a2e",
+    "url": "/static/js/2.721a156e.chunk.js"
+  },
+  {
+    "revision": "f99cd4666b1eae75b039",
+    "url": "/static/js/main.473d3baa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
