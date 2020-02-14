eb8808f6c26c538feac6b6690a8079f6edd1fd51
raffazizzi <raffaeleviglianti@gmail.com>
2020-02-14 18:14:29 -0500

    new iteration

---
+++ precache-manifest.dedb0d32d7b8373cdf9836cd53201a99.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7013a838ba9fbf7b0526",
+    "url": "./static/css/mermtt-viewer.css"
+  },
+  {
+    "revision": "7013a838ba9fbf7b0526",
+    "url": "./static/js/mermtt-viewer.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.173aea1698050940e1927c1fa2a63994.js"
+  "./precache-manifest.dedb0d32d7b8373cdf9836cd53201a99.js"
 );
 
 self.addEventListener('message', (event) => {
